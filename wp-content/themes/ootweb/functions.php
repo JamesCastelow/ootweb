f280fa80807dc40327c151d177ace93bb376010c
James Castelow <jamescastelow@kinocreative.co.uk>
2017-06-12 11:01:00 +0100

    Installed Foundation

--- wp-content/themes/ootweb/functions.php
+++ wp-content/themes/ootweb/functions.php
@@ -1,67 +1,23 @@
 <?php
 add_action( 'after_setup_theme', 'ootweb_setup' );
-function ootweb_setup()
-{
-load_theme_textdomain( 'ootweb', get_template_directory() . '/languages' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-global $content_width;
-if ( ! isset( $content_width ) ) $content_width = 640;
-register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'ootweb' ) )
-);
+function ootweb_setup() {
+	load_theme_textdomain( 'ootweb', get_template_directory() . '/languages' );
+	register_nav_menus( array( 'main-menu' => __( 'Main Menu', 'ootweb' ) ) );
 }
+
 add_action( 'wp_enqueue_scripts', 'ootweb_load_scripts' );
-function ootweb_load_scripts()
-{
-wp_enqueue_script( 'jquery' );
-}
-add_action( 'comment_form_before', 'ootweb_enqueue_comment_reply_script' );
-function ootweb_enqueue_comment_reply_script()
-{
-if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
-}
-add_filter( 'the_title', 'ootweb_title' );
-function ootweb_title( $title ) {
-if ( $title == '' ) {
-return '&rarr;';
-} else {
-return $title;
-}
-}
-add_filter( 'wp_title', 'ootweb_filter_wp_title' );
-function ootweb_filter_wp_title( $title )
-{
-return $title . esc_attr( get_bloginfo( 'name' ) );
-}
-add_action( 'widgets_init', 'ootweb_widgets_init' );
-function ootweb_widgets_init()
-{
-register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'ootweb' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
-function ootweb_custom_pings( $comment )
-{
-$GLOBALS['comment'] = $comment;
-?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php 
-}
-add_filter( 'get_comments_number', 'ootweb_comments_number' );
-function ootweb_comments_number( $count )
-{
-if ( !is_admin() ) {
-global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
+function ootweb_load_scripts() {
+	wp_enqueue_style('appcss', get_template_directory_uri() . '/css/app.css');
+	wp_enqueue_style('foundationcss', get_template_directory_uri() . '/css/foundation.min.css');
+	
+	wp_enqueue_script('cdnjquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '1.0.0', false);
+	wp_enqueue_script('whatinput', get_template_directory_uri() . '/js/vendor/what-input.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script('foundation', get_template_directory_uri() . '/js/vendor/foundation.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script('appjs', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true);
+	
+	wp_enqueue_script('ootjs', get_template_directory_uri() . '/js/oot.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script( 'wp-util' );
+	
 }
